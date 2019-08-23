42d55c6ca750de78329adb7dcdc3e4a87896b063
EduardoUV <eduardomorales.c01@hotmail.com>
2019-08-23 18:23:18 -0500

    Actualización

--- SEyGRE/ClientApp/src/app/citizen/cmaps/cmaps.component.ts
+++ SEyGRE/ClientApp/src/app/citizen/cmaps/cmaps.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ViewChild, NgZone } from '@angular/core';
+import { Component, ViewChild, NgZone, ElementRef, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { MapsAPILoader, AgmMap } from '@agm/core';
-import { GoogleMapsAPIWrapper } from '@agm/core/services';
+import { MapsAPILoader} from '@agm/core';
 
 
 @Component({
@@ -9,10 +8,93 @@ import { GoogleMapsAPIWrapper } from '@agm/core/services';
   templateUrl: './cmaps.component.html',
   styleUrls: ['./cmaps.component.css']
 })
-export class CMapsComponent {
 
-  public d: datas[];
+export class CMapsComponent implements OnInit {
+
+  latitude: number;
+  longitude: number;
+  address: string;
+
+  private geoCoder;
+
+  @ViewChild('search')
+  public searchElementRef: ElementRef;
+
+  constructor (private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) { }
   
+
+  ngOnInit() {
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+      this.geoCoder = new google.maps.Geocoder;
+
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["address"]
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          
+        });
+      });
+    });
+  }
+
+  // Get Current Location Coordinates
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+     
+        this.getAddress(this.latitude, this.longitude);
+      });
+    }
+  }
+
+
+  markerDragEnd($event: MouseEvent) {
+    console.log($event);
+   // this.latitude = $event.coords.lat;
+   // this.longitude = $event.coords.lng;
+    this.getAddress(this.latitude, this.longitude);
+  }
+
+  getAddress(latitude, longitude) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
+      console.log(results);
+      console.log(status);
+      if (status === 'OK') {
+        if (results[0]) {
+      
+          this.address = results[0].formatted_address;
+        } else {
+          console.log('No se encontrador resultados');
+        }
+      } else {
+       console.log('Geocodificacion fallo: ' + status);
+      }
+
+    });
+  }
+
+}
+
+  //public d: datas[];
+
+  /*
+
   constructor(private http: HttpClient) {
 
     this.http.get<datas[]>("api/CentrosAcopio/ObtenerUbicacionCentros").subscribe(result => {
@@ -32,3 +114,4 @@ interface datas {
 }
 
 
+*/
